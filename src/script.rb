027f7cba057c688c13d9227b915739a7462fabb6
lowentropy <lowentropy@gmail.com>
2008-01-05 18:33:53 +0000

    - updated script for sandbox

--- src/script.rb
+++ src/script.rb
@@ -1,47 +1,50 @@
-$:.unshift File.dirname(__FILE__)
 require 'sandbox'
 
 class Script
+
+	module Definers
+		def reset
+			@state = nil
+			@states = {}
+		end
+		def state(name, &block)
+			@states[name.to_sym] = {}
+			@state = name.to_sym
+			yield
+		end
+		def method_missing(id, *args, &block)
+			return super if args.any? or @state.nil?
+			@states[@state][id.id2name.to_sym] = block
+		end
+	end
+
 	def initialize
 		@sandbox = Sandbox.safe
-		@sandbox.eval(%{
-			def reset
-				@state = nil
-				@states = {}
-			end
-			def state(name, &block)
-				@states[name.to_sym] = {}
-				@state = name.to_sym
-				yield
-			end
-			def method_missing(id, *args, &block)
-				return super if args.any? or @state.nil?
-				@states[@state][id.id2name.to_sym] = block
-			end
-			reset
-		})
+		@sandbox.import Script::Definers
+		@sandbox.eval 'class << self; include Script::Definers; end; reset'
 	end
 	def eval(str)
 		@sandbox.eval str, :safelevel => 3, :timeout => 5
 	end
-	def run
-		states = @sandbox.eval '@states'
-		@sandbox.reset
-		states.each do |state,events|
-			events.each do |event,block|
-				@sandbox.eval %{
-					((@states||={})[:#{state}]||={})[:#{event}] = #{block.to_ruby}
-				}
-			end
+	module Runners
+		def goto(new_state)
+			raise 'bad state' unless @states[new_state]
+			@state = new_state
+			throw :goto
 		end
+		def method_missing(id, *args)
+			raise "no such method #{id.id2name}"
+		end
+		def state
+			@state
+		end
+	end
+	def run
 		@sandbox.eval %{
-			def goto(new_state)
-				raise 'bad state' unless @states[new_state]
-				@state = new_state
-				throw :goto
-			end
 			@state = @states.keys.first
 		}
+		@sandbox.import Script::Runners
+		@sandbox.eval 'class << self; include Script::Runners; def reset; end; end; nil'
 		@sandbox.eval %{
 			while true
 				event = :start
